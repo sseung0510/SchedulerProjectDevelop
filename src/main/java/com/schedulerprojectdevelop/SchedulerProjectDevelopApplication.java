807ac0be3d73027329d2486613f0fd8e3b31eb28
sseung0510 <choiseunghi0510@gmail.com>
2025-11-11 14:27:30 +0900

    일정 관리 앱 develop

---
+++ src/main/java/com/schedulerprojectdevelop/SchedulerProjectDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.schedulerprojectdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerProjectDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerProjectDevelopApplication.class, args);
+    }
+
+}
